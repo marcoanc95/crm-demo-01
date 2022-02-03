7f60d38b9577917243f310f6afbc4a00aad78620
Marco Antonio Navarro Camarena <marcoanc95@gmail.com>
2022-02-03 16:43:24 -0600

    12

--- crm_cuota/models/crm_lead.py
+++ crm_cuota/models/crm_lead.py
@@ -5,19 +5,21 @@ from datetime import date, datetime
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    cuota = fields.Float('Couta')
-    cuota_cubierta = fields.Float('Couta cubierta')
-    cuota_restante = fields.Float('Couta restante')
+    cuota = fields.Float('Couta', compute='_compute_cuota')
+    cuota_cubierta = fields.Float('Couta cubierta', compute='_compute_cuota')
+    cuota_restante = fields.Float('Couta restante', compute='_compute_cuota')
 
-    @api.onchange('expected_revenue', 'stage_id')
-    def _on_change_cuota(self):
+    @api.depends('expected_revenue', 'stage_id')
+    def _compute_cuota(self):
         fechaActual = datetime.today().date()
         linea_cuota = self.sudo().env['crm.cuota'].search([('user_id', '=', self.user_id.id), ('team_id', '=', self.team_id.id), ('fecha_inicio', '<=', fechaActual), ('fecha_fin', '>=', fechaActual)])
+        
         fechaInicio = linea_cuota[0].fecha_inicio
         fechaFin = linea_cuota[0].fecha_fin
+        
         op_ganadas = self.sudo().env['crm.lead'].search([('user_id', '=', self.user_id.id), ('team_id', '=', self.team_id.id), ('won_status', '=', 'won'), ('date_closed', '>=', fechaInicio), ('date_closed', '<=', fechaFin)])
+        
         avance = 0
-
         for op in op_ganadas:
             avance = avance + op.expected_revenue
 
