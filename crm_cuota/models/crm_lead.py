97c73205dea84536cdddfa4506cf046a43d5f016
Marco Antonio Navarro Camarena <marcoanc95@gmail.com>
2022-02-03 16:21:43 -0600

    8

--- crm_cuota/models/crm_lead.py
+++ crm_cuota/models/crm_lead.py
@@ -12,21 +12,20 @@ class CrmLead(models.Model):
     def write(self, vals):
         res = super(CrmLead, self).write(vals)
 
-        for reg in self:
-            fechaActual = datetime.today().date()
-            linea_cuota = reg.sudo().env['crm.cuota'].search([('user_id', '=', reg.user_id.id), ('team_id', '=', reg.team_id.id), ('fecha_inicio', '<=', fechaActual), ('fecha_fin', '>=', fechaActual)])
-            fechaInicio = linea_cuota[0].fecha_inicio
-            fechaFin = linea_cuota[0].fecha_fin
-            op_ganada = reg.sudo().env['crm.lead'].search([('user_id', '=', reg.user_id.id), ('team_id', '=', reg.team_id.id), ('won_status', '<=', 'won'), ('date_closed', '>=', fechaInicio), ('date_closed', '<=', fechaFin)])
-            avance = 0
-            for op in op_ganada:
-                avance = avance + op.expected_revenue
+        fechaActual = datetime.today().date()
+        linea_cuota = self.sudo().env['crm.cuota'].search([('user_id', '=', self.user_id.id), ('team_id', '=', self.team_id.id), ('fecha_inicio', '<=', fechaActual), ('fecha_fin', '>=', fechaActual)])
+        fechaInicio = linea_cuota[0].fecha_inicio
+        fechaFin = linea_cuota[0].fecha_fin
+        op_ganadas = self.sudo().env['crm.lead'].search([('user_id', '=', self.user_id.id), ('team_id', '=', self.team_id.id), ('won_status', '=', 'won'), ('date_closed', '>=', fechaInicio), ('date_closed', '<=', fechaFin)])
+        avance = 0
+        for op in op_ganadas:
+            avance = avance + op.expected_revenue
 
-            reg.update({
-                    'cuota': linea_cuota.cuota,
-                    'cuota_cubierta': avance,
-                    'cuota_restante': linea_cuota.cuota - avance,
-                })
+        self.update({
+                'cuota': linea_cuota.cuota,
+                'cuota_cubierta': avance,
+                'cuota_restante': linea_cuota.cuota - avance,
+            })
 
         return res
 
